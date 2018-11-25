2217b713ea41331ae4b36759de55453713f1a638
wallacevmarques <wallacevmarques@gmail.com>
2018-11-25 15:03:32 -0200

    Remocao de código nao utilizado e mudanca nas URLs dos metodos da API Web

--- src/com/wallace/desafio/StarwarsPlanetsAPI.java
+++ src/com/wallace/desafio/StarwarsPlanetsAPI.java
@@ -1,19 +1,15 @@
 package com.wallace.desafio;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
-import java.util.HashMap;
 
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 import org.bson.Document;
@@ -22,12 +18,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
@@ -35,87 +25,16 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 
 import static com.mongodb.client.model.Filters.*;
-import com.mongodb.client.model.CreateCollectionOptions;
-import com.mongodb.client.model.ValidationOptions;
 import com.mongodb.client.result.DeleteResult;
-import com.mongodb.BasicDBObject;
 
-@Path("zipservice")
-public class ZipCodeService {
+@Path("starwarsplanetsapi")
+public class StarwarsPlanetsAPI {
 	
+	//Lists all the planets on DB
+	//Receives no input
+	//Returns planets info in the form of a JSON Array
 	@GET
-	@Path("/test")
-	public Response test() {
-		
-		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-		MongoDatabase database = mongoClient.getDatabase("starwars");
-		MongoCollection<Document> collection = database.getCollection("planets");
-		
-		MongoIterable<Document> documents = collection.find();
-		
-		for (Document document:documents) {
-			System.out.println(document.getString("name"));
-		}
-		 
-		
-		mongoClient.close();
-		
-		String planetsString = null;
-		
-		return Response.ok().entity(planetsString).build();
-		
-	}
-	
-	@GET
-	@Path("/listPlanetsTest")
-	public Response listPlanetsTest() {
-		
-		Response response = Response.serverError().build();
-		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-		
-		try {
-			
-			MongoDatabase database = mongoClient.getDatabase("starwars");
-			MongoCollection<Document> collection = database.getCollection("planets");
-			
-			MongoIterable<Document> planets = collection.find();
-			
-			JSONArray planetsJSON = new JSONArray();
-			
-			if (planets != null) {
-				
-				for (Document planet:planets) {
-					
-					JSONObject planetJSON = new JSONObject();
-					planetJSON.put("id", planet.getObjectId("_id"));
-					planetJSON.put("name", planet.getString("name"));
-					planetJSON.put("climate", planet.getString("climate"));
-					planetJSON.put("terrain", planet.getString("terrain"));
-					
-					planetsJSON.put(planetJSON);
-				}
-				
-				response = Response.ok().entity(planetsJSON.toString()).build();
-				
-			} else {
-				
-				response = Response.ok().entity(null).build();
-				
-			}
-			
-		} catch (Exception e) {
-			
-			e.printStackTrace();
-			
-		}
-		
-		mongoClient.close();
-		return response;
-		
-	}
-	
-	@GET
-	@Path("/listPlanets")
+	@Path("/listplanets")
 	public Response listPlanets() {
 		
 		//Opens connection with database
@@ -169,8 +88,11 @@ public class ZipCodeService {
 		
 	}
 	
+	//Searches DB for a planet with inputed name
+	//Receives the name of the planet in the form of a string parameter on the URL
+	//Returns planet info in the form of a JSON
 	@GET
-	@Path("/searchName/{name}")
+	@Path("/searchbyname/{name}")
 	public Response searchName(@PathParam("name") String name) {
 		
 		//Opens connection with database
@@ -214,8 +136,11 @@ public class ZipCodeService {
 		
 	}
 	
+	//Searches DB for a planet with inputed id
+	//Receives the id of the planet in the form of a string parameter on the URL
+	//Returns planet info in the form of a JSON
 	@GET
-	@Path("/searchID/{id}")
+	@Path("/searchbyid/{id}")
 	public Response searchID(@PathParam("id") String id) {
 		
 		//Opens connection with database
@@ -260,8 +185,11 @@ public class ZipCodeService {
 		
 	}
 	
+	//Deletes a planet with inputed id from DB
+	//Receives the id of the planet in the form of a string parameter on the URL
+	//Returns result of the delete on the HTTP status code
 	@DELETE
-	@Path("/deletePlanet/{id}")
+	@Path("/deleteplanet/{id}")
 	public Response deletePlanet(@PathParam("id") String id) {
 		
 		//Opens connection with database
@@ -310,8 +238,11 @@ public class ZipCodeService {
 		
 	}
 	
+	//Adds a planet with inputed to DB
+	//Receives JSON containing name, climate and terrain of the planet
+	//Returns result of the addition on the HTTP status code
 	@POST
-	@Path("/addPlanet/")
+	@Path("/addplanet/")
 	public Response addPlanet(String inputPlanetJSONString) {
 		
 		try {
@@ -406,64 +337,6 @@ public class ZipCodeService {
 		}
 		//problem, mongo connection not always closed
 		return null;
-		
-	}
-	
-	
-	
-	@GET
-	@Path("/loockup/{zipcode}/{extension}")
-	public Response loockup(@PathParam("zipcode") String zipcode, @PathParam("extension") String ext) throws JsonProcessingException {
-		
-		if (zipcode == null || zipcode.length() < 5) {
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("zip code missing or not long enough").build();
-		}
-		
-		City city = new City();
-		city.setZip(zipcode + "-" + ext);
-		city.setCityName("New York");
-		city.setLatitude("45.3");
-		city.setLongitude("45.2");
-		
-		XmlMapper xmlMapper = new XmlMapper();
-		String xmlString = xmlMapper.writeValueAsString(city);
-		
-		return Response.ok().entity(xmlString).build();
-		
-	}
-	
-	@POST
-	@Path("/add")
-	public Response addZipCode(MultivaluedMap<String, String> formParams) {
-		
-		String zipcode = formParams.getFirst("zipcode");
-		String note = formParams.getFirst("note");
-		
-		return Response.ok().entity("received: " + zipcode + "-" + note).build();
-		
-	}
-	
-	@POST
-	@Path("/addwithjson")
-	public Response addWithJson(String jsonInputString) throws JsonParseException, JsonMappingException, IOException {
-		
-		ObjectMapper objectMapper = new ObjectMapper();
-		City city = objectMapper.readValue(jsonInputString, City.class);
-		
-		return Response.ok().entity("Returning city: " + city.getCityName() + "-" + city.getZip()).build();
-		
-	}
-	
-	@POST
-	@Path("/addwithjsonstring")
-	public Response addWithJsonString(String jsonInputString) throws JsonParseException, JsonMappingException, IOException {
-		
-		HashMap<String, String> result = new ObjectMapper().readValue(jsonInputString, HashMap.class);
-		
-		String cityName = result.get("cityName");
-		String zip = result.get("zip");
-		
-		return Response.ok().entity("Returning city: " + cityName + "-" + zip).build();
 		
 	}
 
