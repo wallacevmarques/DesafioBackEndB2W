6b04e72b67d13bd8d2e29e0c4273f413ccc39620
wallacevmarques <wallacevmarques@gmail.com>
2018-11-25 13:44:59 -0200

    Metodo de adicao de planetas e obtencao de numero de filmes nos metodos de busca e listagem de planetas

--- src/com/wallace/desafio/ZipCodeService.java
+++ src/com/wallace/desafio/ZipCodeService.java
@@ -1,6 +1,11 @@
 package com.wallace.desafio;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.HashMap;
 
 import javax.ws.rs.DELETE;
@@ -113,45 +118,50 @@ public class ZipCodeService {
 	@Path("/listPlanets")
 	public Response listPlanets() {
 		
+		//Opens connection with database
 		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
 		
 		try {
-			mongoClient.listDatabases().first();
-		} catch (Exception e) {
-			return Response.serverError().build();
-		}
-		
-		MongoDatabase database = mongoClient.getDatabase("starwars");
-		MongoCollection<Document> collection = database.getCollection("planets");
-		
-		MongoIterable<Document> planets = collection.find();
-		
-		if (planets.first() == null) {
-			mongoClient.close();
-			return Response.noContent().build();
-		}
-		
-		JSONArray planetsJSON = new JSONArray();
 			
-		try {
-		
+			//Gets the collection
+			MongoDatabase database = mongoClient.getDatabase("starwars");
+			MongoCollection<Document> collection = database.getCollection("planets");
+			
+			//Looks for planets
+			MongoIterable<Document> planets = collection.find();
+			
+			//Verifies if there's a planet in the search result
+			if (planets.first() == null) {
+				mongoClient.close();
+				return Response.noContent().build();
+			}
+			
+			//Instantiate JSON Array for storing planets
+			JSONArray planetsJSON = new JSONArray();
+			
+			//Fills the JSON Array with the planets
 			for (Document planet:planets) {
 				
+				//Creates a JSON Object of a planet and copies the fields of current planet
 				JSONObject planetJSON = new JSONObject();
 				planetJSON.put("id", planet.getObjectId("_id"));
 				planetJSON.put("name", planet.getString("name"));
 				planetJSON.put("climate", planet.getString("climate"));
 				planetJSON.put("terrain", planet.getString("terrain"));
+				planetJSON.put("number_of_films", planet.getInteger("number_of_films"));
 				
+				//Adds JSON of current planet to the JSON Array of planets
 				planetsJSON.put(planetJSON);
 			}
 			
+			//Closes connection and sends response
 			mongoClient.close();
 			return Response.ok().entity(planetsJSON.toString()).build();
 		
-		} catch (Exception e) {
+		} catch (Exception exception) {
 			
-			e.printStackTrace();
+			//Closes connection and sends response
+			exception.printStackTrace();
 			mongoClient.close();
 			return Response.serverError().build();
 			
@@ -163,41 +173,44 @@ public class ZipCodeService {
 	@Path("/searchName/{name}")
 	public Response searchName(@PathParam("name") String name) {
 		
-		Response response = Response.serverError().build();
+		//Opens connection with database
 		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
 		
 		try {
 			
+			//Gets the collection
 			MongoDatabase database = mongoClient.getDatabase("starwars");
 			MongoCollection<Document> collection = database.getCollection("planets");
 			
+			//Looks for a planet with inserted name
 			Document planet = collection.find(eq("name", name)).first();
-
-			JSONObject planetJSON = new JSONObject();
 			
-			if (planet != null) {
-				
-				planetJSON.put("id", planet.getObjectId("_id"));
-				planetJSON.put("name", planet.getString("name"));
-				planetJSON.put("climate", planet.getString("climate"));
-				planetJSON.put("terrain", planet.getString("terrain"));
-				
-				response = Response.ok().entity(planetJSON.toString()).build();
-				
-			} else {
-				
-				response = Response.ok().entity(null).build();
-				
+			//Verifies if there's a planet in the search result
+			if (planet == null) {
+				mongoClient.close();
+				return Response.noContent().build();
 			}
 			
-		} catch (Exception e) {
+			//Creates a JSON Object of a planet and copies the fields of the found planet
+			JSONObject planetJSON = new JSONObject();
+			planetJSON.put("id", planet.getObjectId("_id"));
+			planetJSON.put("name", planet.getString("name"));
+			planetJSON.put("climate", planet.getString("climate"));
+			planetJSON.put("terrain", planet.getString("terrain"));
+			planetJSON.put("number_of_films", planet.getInteger("number_of_films"));
 			
-			e.printStackTrace();
+			//Closes connection and sends response
+			mongoClient.close();
+			return Response.ok().entity(planetJSON.toString()).build();
+			
+		} catch (Exception exception) {
+			
+			//Closes connection and sends response
+			exception.printStackTrace();
+			mongoClient.close();
+			return Response.serverError().build();
 			
 		}
-		
-		mongoClient.close();
-		return response;
 		
 	}
 	
@@ -205,42 +218,45 @@ public class ZipCodeService {
 	@Path("/searchID/{id}")
 	public Response searchID(@PathParam("id") String id) {
 		
-		Response response = Response.serverError().build();
+		//Opens connection with database
 		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
 		
 		try {
 			
+			//Gets the collection
 			MongoDatabase database = mongoClient.getDatabase("starwars");
 			MongoCollection<Document> collection = database.getCollection("planets");
 			
+			//Looks for a planet with inserted id
 			ObjectId objectId = new ObjectId(id);
 			Document planet = collection.find(eq("_id", objectId)).first();
 
-			JSONObject planetJSON = new JSONObject();
-			
-			if (planet != null) {
-				
-				planetJSON.put("id", planet.getObjectId("_id"));
-				planetJSON.put("name", planet.getString("name"));
-				planetJSON.put("climate", planet.getString("climate"));
-				planetJSON.put("terrain", planet.getString("terrain"));
-				
-				response = Response.ok().entity(planetJSON.toString()).build();
-				
-			} else {
-				
-				response = Response.ok().entity(null).build();
-				
+			//Verifies if there's a planet in the search result
+			if (planet == null) {
+				mongoClient.close();
+				return Response.noContent().build();
 			}
 			
-		} catch (Exception e) {
+			//Creates a JSON Object of a planet and copies the fields of the found planet
+			JSONObject planetJSON = new JSONObject();
+			planetJSON.put("id", planet.getObjectId("_id"));
+			planetJSON.put("name", planet.getString("name"));
+			planetJSON.put("climate", planet.getString("climate"));
+			planetJSON.put("terrain", planet.getString("terrain"));
+			planetJSON.put("number_of_films", planet.getInteger("number_of_films"));
 			
-			e.printStackTrace();
+			//Closes connection and sends response
+			mongoClient.close();
+			return Response.ok().entity(planetJSON.toString()).build();
+			
+		} catch (Exception exception) {
+			
+			//Closes connection and sends response
+			exception.printStackTrace();
+			mongoClient.close();
+			return Response.serverError().build();
 			
 		}
-		
-		mongoClient.close();
-		return response;
 		
 	}
 	
@@ -248,45 +264,148 @@ public class ZipCodeService {
 	@Path("/deletePlanet/{id}")
 	public Response deletePlanet(@PathParam("id") String id) {
 		
-		Response response = Response.serverError().build();
+		//Opens connection with database
 		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
 		
 		try {
 			
+			//Gets the collection
 			MongoDatabase database = mongoClient.getDatabase("starwars");
 			MongoCollection<Document> collection = database.getCollection("planets");
 			
+			//Looks for a planet with inserted id
 			ObjectId objectId = new ObjectId(id);
 			Document planet = collection.find(eq("_id", objectId)).first();
 			
-			if (planet != null) {
+			//Verifies if there's a planet in the search result
+			if (planet == null) {
+				mongoClient.close();
+				return Response.noContent().build();
+			}
+			
+			//Tries to delete the planet	
+			DeleteResult deleteResult = collection.deleteOne(planet);
+			if(deleteResult.wasAcknowledged()) {
 				
-				DeleteResult deleteResult = collection.deleteOne(planet);
-				
-				if(deleteResult.wasAcknowledged()) {
-					
-					response = Response.ok().build();
-					
-				} else {
-					
-					response = Response.serverError().build();
-					
-				}
+				//Closes connection and sends response
+				mongoClient.close();
+				return Response.ok().build();
 				
 			} else {
 				
-				response = Response.noContent().build();
+				//Closes connection and sends response
+				mongoClient.close();
+				return Response.serverError().build();
 				
 			}
 			
-		} catch (Exception e) {
+		} catch (Exception exception) {
 			
-			e.printStackTrace();
+			//Closes connection and sends response
+			exception.printStackTrace();
+			mongoClient.close();
+			return Response.serverError().build();
 			
 		}
 		
-		mongoClient.close();
-		return response;
+	}
+	
+	@POST
+	@Path("/addPlanet/")
+	public Response addPlanet(String inputPlanetJSONString) {
+		
+		try {
+			
+			URL url = new URL("https://swapi.co/api/planets/");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+			connection.setRequestProperty("Accept", "application/json");
+			
+			int status = connection.getResponseCode();
+			if (status != 200) {
+				
+			}
+			
+			BufferedReader responseBuffer = new BufferedReader(
+					  new InputStreamReader(connection.getInputStream()));
+			String responseLine;
+			StringBuffer response = new StringBuffer();
+			while ((responseLine = responseBuffer.readLine()) != null) {
+			    response.append(responseLine);
+			}
+			responseBuffer.close();
+			
+			//Gets array of planets from SWAPI
+			JSONObject jsonObjectResponse = new JSONObject(response.toString());
+			JSONArray planetsArray = jsonObjectResponse.getJSONArray("results");
+			
+			//Parses inputPlanet to JSONObject
+			JSONObject inputPlanetJSON = new JSONObject(inputPlanetJSONString);
+			
+			//Verifies if there's a star wars planet with the name of the to be inserted one
+			boolean planetExists = false;
+			JSONObject planet = new JSONObject();
+			int count = planetsArray.length();
+			for (int i = 0; i < count; i++) {
+				
+				planet = planetsArray.getJSONObject(i);
+				if (planet.getString("name").equals(inputPlanetJSON.getString("name"))) {
+					
+					planetExists = true;
+					break;
+					
+				}
+				
+			}
+			
+			//Return no content if no planet with such name was found
+			if (!planetExists) {
+				return Response.noContent().build();
+			}
+			
+			//Prepare new planet JSON using user entered and SWAPI data 
+			int numberOfFilms = planet.getJSONArray("films").length();
+			JSONObject newPlanetJSON = new JSONObject();
+			newPlanetJSON.put("name", inputPlanetJSON.getString("name"));
+			newPlanetJSON.put("climate", inputPlanetJSON.getString("climate"));
+			newPlanetJSON.put("terrain", inputPlanetJSON.getString("terrain"));
+			newPlanetJSON.put("number_of_films", numberOfFilms);
+			
+			//Opens connection with database
+			MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+			
+			//Gets the collection
+			MongoDatabase database = mongoClient.getDatabase("starwars");
+			MongoCollection<Document> collection = database.getCollection("planets");
+			
+			//Looks for a planet with inserted name
+			Document verifyPlanet = collection.find(eq("name", inputPlanetJSON.getString("name"))).first();
+			
+			//Verifies if there's already a planet on db with the name of the to be inserted one
+			if (verifyPlanet != null) {
+				mongoClient.close();
+				return Response.ok().build();
+			}
+			
+			//Parses newPlanetJSON to document in order to add it to mongo db collection 
+			Document newPlanet = Document.parse(newPlanetJSON.toString());
+			collection.insertOne(newPlanet);
+			
+			//problem, mongo connection not always closed
+			mongoClient.close();
+			return Response.created(null).build();
+			
+			//problem, mongo connection not always closed
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//problem, mongo connection not always closed
+		return null;
 		
 	}
 	
